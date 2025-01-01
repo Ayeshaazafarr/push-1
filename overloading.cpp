03386730aaec6a8c17cfa6104123f1e4c8f682a3
Ayeshaazafarr <ayeshazafarr.0@gmail.com>
2025-01-01 22:05:17 +0500

    day 1 of daily pushing

---
+++ overloading.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+	
+	#include<iostream>
+using namespace std;
+
+	
+	void info(int x , int y){
+		cout<<x+y;
+}
+	void info(double x , int y){
+		cout<<x+y;
+	}
+	int main(){
+		
+		info(5.5 , 9);
+		
+		return 0;
+	}
